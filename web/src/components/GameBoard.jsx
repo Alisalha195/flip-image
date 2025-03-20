f20ff90179382fb3d675e85f289263611aeda886
Alisalha195 <alisalha195@gmail.com>
2025-03-20 11:06:02 -0700

    fix:ImageCard Component (level parameter)

--- web/src/components/GameBoard.jsx
+++ web/src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import ImageCard from './ImageCard'
 
 import image1 from "../assets/images/1.jpg"
@@ -29,117 +29,117 @@ import image16 from "../assets/images/16.jpg"
 import image17 from "../assets/images/17.jpg"
 import image18 from "../assets/images/18.jpg"
 
-const GameBoard = ({level,setWrongFlips,shuffeledImages,setShuffeledImages,imagesBoard,setImagesBoard,gameFinished,setGameFinished}) => {
+const GameBoard = ({ level, setWrongFlips, shuffeledImages, setShuffeledImages, imagesBoard, setImagesBoard, gameFinished, setGameFinished }) => {
 
-	const initialCompareSet = [
-		{name:"first",item:'0',active:false},
-		{name:"second",item:'99',active:false}  
-	];
+   const initialCompareSet = [
+      { name: "first", item: '0', active: false },
+      { name: "second", item: '99', active: false }
+   ];
 
-	const [compareSet ,setCompareSet] = useState(initialCompareSet);
-	const [allowPress, setAllowPress] = useState(true)
-	const [waiting, setWaiting] = useState(false)
-	
-	// compare cards
-	useEffect(()=> {  
-		compare(compareSet[0].item, compareSet[1].item)
-		testGameComplete();
-	},[compareSet[0].active , compareSet[1].active ]);
+   const [compareSet, setCompareSet] = useState(initialCompareSet);
+   const [allowPress, setAllowPress] = useState(true)
+   const [waiting, setWaiting] = useState(false)
 
-	// Game Finished
-	useEffect(()=> {
-		if(gameFinished) {
-		  console.log("Game Finished");
-		}
-	},[gameFinished]);
+   // compare cards
+   useEffect(() => {
+      compare(compareSet[0].item, compareSet[1].item)
+      testGameComplete();
+   }, [compareSet[0].active, compareSet[1].active]);
 
-	// ======================================================
-	const easyImagesSources = [
-		image1, image2, image3, image4, image5, image6, image7, image8
-	];
-	const hardImagesSources = [
-		image01, image02, image03, image04, image5, image06, image07, image08,image09 ,image10 ,image11 ,image12 ,image13 ,image14, image15 , image16, image17, image18
-	];
+   // Game Finished
+   useEffect(() => {
+      if (gameFinished) {
+         console.log("Game Finished");
+      }
+   }, [gameFinished]);
 
-	const testGameComplete = () => {
-	    let finished = true;
-	    imagesBoard.map(item => {
-	      if (!item.done) 
-	        finished = false;
-	    });
-	    setGameFinished(finished);
-	}
+   // ======================================================
+   const easyImagesSources = [
+      image1, image2, image3, image4, image5, image6, image7, image8
+   ];
+   const hardImagesSources = [
+      image01, image02, image03, image04, image5, image06, image07, image08, image09, image10, image11, image12, image13, image14, image15, image16, image17, image18
+   ];
 
-	const compare = (firstItem, secondItem) => {
-		if((firstItem != "0") &&(secondItem != "99") ) {
+   const testGameComplete = () => {
+      let finished = true;
+      imagesBoard.map(item => {
+         if (!item.done)
+            finished = false;
+      });
+      setGameFinished(finished);
+   }
 
-		  const firstImgName = firstItem + '.jpg';
-		  const secondImgName = secondItem + '.jpg';
+   const compare = (firstItem, secondItem) => {
+      if ((firstItem != "0") && (secondItem != "99")) {
 
-		  if(firstItem == secondItem) {  
-		    const updatedBoardItem = {imgName:firstImgName, done:true,show:true};
-		    setImagesBoard(imagesBoard.map((boardItem)=>(boardItem.imgName == firstImgName ? updatedBoardItem : boardItem)));
-		    reset();
-		  } else {
-		    setWrongFlips(prev => prev + 1);
-		    const updatedBoardFirstItem = {imgName:firstImgName, done:false, show:false};
-		    const updatedBoardSecondItem = {imgName:secondImgName, done:false,show:false};
-		    setTimeout(()=> {
-		      setImagesBoard(imagesBoard.map((boardItem)=>(boardItem.imgName == firstImgName 
-		          ? updatedBoardFirstItem 
-		          : boardItem.imgName == secondImgName 
-		              ? updatedBoardSecondItem 
-		              : boardItem
-		      )));
-		    },700);
-		    reset();
-		  }
-		}
-    }
+         const firstImgName = firstItem + '.jpg';
+         const secondImgName = secondItem + '.jpg';
 
-	const reset = () => {
-		setTimeout(()=> {
-		  setWaiting(false);
-		  setCompareSet(initialCompareSet);
-		},1000);
+         if (firstItem == secondItem) {
+            const updatedBoardItem = { imgName: firstImgName, done: true, show: true };
+            setImagesBoard(imagesBoard.map((boardItem) => (boardItem.imgName == firstImgName ? updatedBoardItem : boardItem)));
+            reset();
+         } else {
+            setWrongFlips(prev => prev + 1);
+            const updatedBoardFirstItem = { imgName: firstImgName, done: false, show: false };
+            const updatedBoardSecondItem = { imgName: secondImgName, done: false, show: false };
+            setTimeout(() => {
+               setImagesBoard(imagesBoard.map((boardItem) => (boardItem.imgName == firstImgName
+                  ? updatedBoardFirstItem
+                  : boardItem.imgName == secondImgName
+                     ? updatedBoardSecondItem
+                     : boardItem
+               )));
+            }, 700);
+            reset();
+         }
+      }
+   }
 
-		setTimeout(()=>{
-		  setAllowPress(true)
-		},2000)
+   const reset = () => {
+      setTimeout(() => {
+         setWaiting(false);
+         setCompareSet(initialCompareSet);
+      }, 1000);
 
-	}
+      setTimeout(() => {
+         setAllowPress(true)
+      }, 2000)
 
-	const getImageBoardShowItem = (index) => {
-		return imagesBoard[index].show
-	} 
+   }
 
-	return (
-		<div className="p-5 xs:[border:2px] flex justify-center">
-			<div className={level=="easy" ? "grid grid-cols-4 xs:gap-2 md:gap-6" : "grid grid-cols-6 xs:gap-2 md:gap-3"}>
-			{
-				shuffeledImages.map((item,index)=>(
-					<ImageCard 
-						level={level}
-					    key={index} 
-                        src={ level=="easy" ? easyImagesSources[item.substring(0,1)-1] : hardImagesSources[item.substring(0,2)-1]}
-                        itemNumber={level=="easy" ? item.substring(0,1) : item.substring(0,2)}
-                        imagesBoard={imagesBoard}
-                        setImagesBoard={setImagesBoard}
-                        showImageInBoard={getImageBoardShowItem(index)}
-                        index={index}
-                        compareSet={compareSet}
-                        setCompareSet={setCompareSet}
-                        waiting={waiting}
-                        setWaiting={setWaiting}
-                        allowPress={allowPress}
-                        setAllowPress={setAllowPress}
-                    />
-				))
-			}
-					
-			</div>
-			
-		</div>	
-	);
+   const getImageBoardShowItem = (index) => {
+      return imagesBoard[index].show
+   }
+
+   return (
+      <div className="p-5 xs:[border:2px] flex justify-center">
+         <div className={level == "easy" ? "grid grid-cols-4 xs:gap-2 md:gap-6" : "grid grid-cols-6 xs:gap-2 md:gap-3"}>
+            {
+               shuffeledImages.map((item, index) => (
+                  <ImageCard
+                     level={level}
+                     key={index}
+                     src={level == "easy" ? easyImagesSources[item.substring(0, 1) - 1] : hardImagesSources[item.substring(0, 2) - 1]}
+                     itemNumber={level == "easy" ? item.substring(0, 1) : item.substring(0, 2)}
+                     imagesBoard={imagesBoard}
+                     setImagesBoard={setImagesBoard}
+                     showImageInBoard={getImageBoardShowItem(index)}
+                     index={index}
+                     compareSet={compareSet}
+                     setCompareSet={setCompareSet}
+                     waiting={waiting}
+                     setWaiting={setWaiting}
+                     allowPress={allowPress}
+                     setAllowPress={setAllowPress}
+                  />
+               ))
+            }
+
+         </div>
+
+      </div>
+   );
 }
 export default GameBoard;
--- web/src/components/ImageCard.jsx
+++ web/src/components/ImageCard.jsx
@@ -1,72 +1,72 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import q_mark from "../assets/images/question.jpg"
 
-const ImageCard = ({ src,itemNumber,imagesBoard,setImagesBoard,showImageInBoard,index, compareSet,setCompareSet,waiting,setWaiting,allowPress,setAllowPress}) => {
+const ImageCard = ({ src,level, itemNumber, imagesBoard, setImagesBoard, showImageInBoard, index, compareSet, setCompareSet, waiting, setWaiting, allowPress, setAllowPress }) => {
 
-	// console.log('index' , index);
-	// console.log('itemNumber' , itemNumber);
-	// console.log('src' , src);
+   // console.log('index' , index);
+   // console.log('itemNumber' , itemNumber);
+   // console.log('src' , src);
 
 
-	// compare two current clicked images 
-	const handlePress = () => {
-		// we clicked at least one time
-		if(waiting) {
+   // compare two current clicked images 
+   const handlePress = () => {
+      // we clicked at least one time
+      if (waiting) {
 
-		  // first click occured so we are handling second click
-		  if((compareSet[0].item != '0') && (!compareSet[1].active)) {
+         // first click occured so we are handling second click
+         if ((compareSet[0].item != '0') && (!compareSet[1].active)) {
 
-		    const updatedItem = {name:"second",item:itemNumber, active:true}
-		    setCompareSet( compareSet.map((compareItem) =>(compareItem.name=="second" ? updatedItem : compareItem)));
+            const updatedItem = { name: "second", item: itemNumber, active: true }
+            setCompareSet(compareSet.map((compareItem) => (compareItem.name == "second" ? updatedItem : compareItem)));
 
-		    showImageAtIndex(index);
-		    setAllowPress(false);
-		    // console.log("Second Click");
+            showImageAtIndex(index);
+            setAllowPress(false);
+            // console.log("Second Click");
 
-		  }  
-		// we just clicked for the first time
-		} else {
-			
-		  // we hanlde first click
-		  if((compareSet[0].item == '0') && (!compareSet[0].active)  ) {
-		    // begin Timer (1500 ms) and wait
-		    setWaiting(true);
-		    const updatedItem = {name:"first",item:itemNumber, active:true}
-		    setCompareSet( compareSet.map((compareItem) =>(compareItem.name=="first" ? updatedItem : compareItem)));
-		    showImageAtIndex(index);
-		    // console.log("First Click") 
-		  } 
-		} 
-	};
+         }
+         // we just clicked for the first time
+      } else {
 
-	const showImageAtIndex = (index) =>{
-		const updatedItem = {imgName:(itemNumber+'.jpg'), done:false, show:true};
-		// console.log("updatedItem in showImageAtIndex ",updatedItem)
-		setImagesBoard(imagesBoard.map((imgItem, itemIndex)=>(
-		(itemIndex === index && !imgItem?.clicked ) ? updatedItem : imgItem  
-		) ));
-	}
+         // we hanlde first click
+         if ((compareSet[0].item == '0') && (!compareSet[0].active)) {
+            // begin Timer (1500 ms) and wait
+            setWaiting(true);
+            const updatedItem = { name: "first", item: itemNumber, active: true }
+            setCompareSet(compareSet.map((compareItem) => (compareItem.name == "first" ? updatedItem : compareItem)));
+            showImageAtIndex(index);
+            // console.log("First Click") 
+         }
+      }
+   };
 
-	return (
-			<button className="click [max-height:100vh]  "
-				onClick={handlePress}
-				disabled={showImageInBoard}
-			>
-				<div className={level=="easy" ?"bg-red-300 [border-radius:9%] w-[90px] h-[90px] [overflow:hidden] [border:1px_solid_#ccc]" :
-				"bg-red-300 [border-radius:9%] w-[80px] h-[80px] [overflow:hidden] [border:1px_solid_#ccc]"}>
-					
-					{   showImageInBoard 
-						?   <img  
-								src={src}
-								className="w-[100%] h-[100%]" 
-							/>
-						:   <img  
-								src={q_mark}
-								className="w-[100%] h-[100%]" 
-							/>
-					}
-				</div>
-			</button>
-	);
+   const showImageAtIndex = (index) => {
+      const updatedItem = { imgName: (itemNumber + '.jpg'), done: false, show: true };
+      // console.log("updatedItem in showImageAtIndex ",updatedItem)
+      setImagesBoard(imagesBoard.map((imgItem, itemIndex) => (
+         (itemIndex === index && !imgItem?.clicked) ? updatedItem : imgItem
+      )));
+   }
+
+   return (
+      <button className="click [max-height:100vh]  "
+         onClick={handlePress}
+         disabled={showImageInBoard}
+      >
+         <div className={level == "easy" ? "bg-red-300 [border-radius:9%] w-[90px] h-[90px] [overflow:hidden] [border:1px_solid_#ccc]" :
+            "bg-red-300 [border-radius:9%] w-[80px] h-[80px] [overflow:hidden] [border:1px_solid_#ccc]"}>
+
+            {showImageInBoard
+               ? <img
+                  src={src}
+                  className="w-[100%] h-[100%]"
+               />
+               : <img
+                  src={q_mark}
+                  className="w-[100%] h-[100%]"
+               />
+            }
+         </div>
+      </button>
+   );
 }
 export default ImageCard;
